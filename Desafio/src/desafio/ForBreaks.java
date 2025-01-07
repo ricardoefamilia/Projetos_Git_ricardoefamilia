6abcfd551c91527282c5556a8a215c8358f8507f
Marcelo Farias <marcelo.farias@adapsbrasil.com.br>
2025-01-07 19:11:24 -0300

    upjavacurso

--- Desafio/src/desafio/ForBreaks.java
+++ Desafio/src/desafio/ForBreaks.java
@@ -6,7 +6,7 @@ public class ForBreaks {
 		// Break para laço interno e break rotulado para laço externo.
 		externo: for (int i = 0; i <= 4; i++) {
 			for(int y = 0; y<=2; y++) {
-				if(y==1) {
+				if(y==2) {
 					System.out.printf("i= %d, y= %d", i, y);
 					break externo;
 				}
--- Desafio/src/module-info.java
+++ Desafio/src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module Desafio {
+module Projetos_Git_ricardoefamilia {
 }
